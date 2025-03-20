78cbf56df8e81833c0b425ea818b18a31a30e564
rithwik777222 <rithwikk312@gmail.com>
2025-03-20 08:42:06 +0530

    sum of n natural numbers

---
+++ sum of natural numbers.py
@@ -0,0 +1,2 @@
+total_sum = sum(range(1,11))
+print("the sum of the numbers is", total_sum)
---
+++ sum of number 2.py
@@ -0,0 +1,3 @@
+n = int(input("number up to which natural numbers have to be summed:"))
+total_sum = sum(range(1,n+1))
+print("The sum of the first", n ,"natural numbers is:",total_sum)
